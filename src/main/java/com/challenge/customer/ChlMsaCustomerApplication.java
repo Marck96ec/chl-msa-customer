4a659351833095b8771d67b675d09d9f1cbcbf7b
maclavij <maclavij@pichincha.com>
2024-12-12 17:09:13 -0500

    update test mutation

--- src/main/java/com/challenge/customer/ChlMsaCustomerApplication.java
+++ src/main/java/com/challenge/customer/ChlMsaCustomerApplication.java
@@ -4,9 +4,10 @@ import com.challenge.customer.configuration.ApplicationPropertiesConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-
+@ComponentScan(basePackages = {"com.challenge.customer"})
 @EnableConfigurationProperties({ApplicationPropertiesConfiguration.class})
 public class ChlMsaCustomerApplication {
 
